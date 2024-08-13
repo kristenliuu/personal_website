c4b0d70c7b4f4acd33f5adf63042b11c199779fa
Kristen Liu <kristenyliu@gmail.com>
2024-08-12 20:06:51 -0400

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Portfolio from './components/Portfolio';
-// import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/about" element={<About  />} />
-        {/* <Route path="/contact" element={<Contact  />} /> */}
         <Route path="/portfolio" element={<Portfolio />} />
         </Route>
     </Routes>
